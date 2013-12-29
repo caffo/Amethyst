3d40abc681766462a9157c448985fc7ff41c1767
Rodrigo Franco <caffeine@gmail.com>
2013-12-29 19:10:13 -0200

    Add named keys for arrowkeys

--- Amethyst/AMHotKeyManager.m
+++ Amethyst/AMHotKeyManager.m
@@ -146,6 +146,10 @@ AMKeyCode AMKeyCodeInvalid = 0xFF;
     // are not generated from the keyboard layout data.
     stringToKeyCodes[@"space"] = @[ @(kVK_Space) ];
     stringToKeyCodes[@"enter"] = @[ @(kVK_Return) ];
+    stringToKeyCodes[@"up"]    = @[ @(kVK_UpArrow) ];
+    stringToKeyCodes[@"down"]  = @[ @(kVK_DownArrow) ];
+    stringToKeyCodes[@"left"]  = @[ @(kVK_LeftArrow) ];
+    stringToKeyCodes[@"right"]  = @[ @(kVK_RightArrow) ];
 
     self.stringToKeyCodes = stringToKeyCodes;
 }
